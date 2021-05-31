491c6370e2927149344e19c78313caa3ebce9d77
Dat Dao Man <dat.daoman@grevo.net>
2021-05-31 15:45:21 +0700

    add

---
+++ usage.class-validator2.ts
@@ -0,0 +1,48 @@
+import {
+  validate,
+  validateOrReject,
+  Contains,
+  IsInt,
+  Length,
+  IsEmail,
+  IsFQDN,
+  IsDate,
+  Min,
+  Max,
+  IsNotEmpty,
+  IsOptional,
+} from 'class-validator';
+
+export class Post {
+  @IsNotEmpty()
+  //@Length(10, 20)
+  title: string;
+
+  @IsOptional()
+  @IsEmail()
+  email: string;
+}
+
+let post = new Post();
+post.title = 'Hello'; // should not pass
+
+validate(post).then(errors => {
+  // errors is an array of validation errors
+  if (errors.length > 0) {
+    console.log('validation failed. errors: ', errors);
+  } else {
+    console.log('validation succeed');
+  }
+});
+
+validateOrReject(post).catch(errors => {
+  console.log('Promise rejected (validation failed). Errors: ', errors);
+});
+// or
+async function validateOrRejectExample(input) {
+  try {
+    await validateOrReject(input);
+  } catch (errors) {
+    console.log('Caught promise rejection (validation failed). Errors: ', errors);
+  }
+}
